7d2b7cecda4dce22699e4d5ca922b1591837bc20
Emeka Akashili <chukwuemeka.akashili@gmail.com>
2024-12-15 20:46:04 +0100

    Fix docs next config

--- docs/next.config.mjs
+++ docs/next.config.mjs
@@ -1,11 +1,16 @@
-import nextra from 'nextra'
- 
+/** @type {import('next').NextConfig} */
+import nextra from "nextra";
+
+const nextConfig = {
+  output: "export"
+};
+
 const withNextra = nextra({
-  theme: 'nextra-theme-docs',
-  themeConfig: './theme.config.jsx'
-})
- 
-export default withNextra()
- 
+  theme: "nextra-theme-docs",
+  themeConfig: "./theme.config.jsx"
+});
+
+export default withNextra(nextConfig);
+
 // If you have other Next.js configurations, you can pass them as the parameter:
 // export default withNextra({ /* other next.js config */ })
