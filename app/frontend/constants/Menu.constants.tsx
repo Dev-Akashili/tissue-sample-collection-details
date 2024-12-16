d08939e287fe2b32c63b9cd7a6c12c1cc81ecf44
Emeka Akashili <chukwuemeka.akashili@gmail.com>
2024-12-16 04:18:06 +0100

    Add constants

---
+++ app/frontend/constants/Menu.constants.tsx
@@ -0,0 +1,25 @@
+import { ROUTES } from "./routes";
+import { MdHome, MdScience } from "react-icons/md";
+import { FaDatabase, FaFileAlt, FaGithub } from "react-icons/fa";
+
+interface NavbarLink {
+  name: string;
+  link: string;
+  icon: React.ReactNode;
+}
+
+export const NAVBAR_LINKS: NavbarLink[] = [
+  { name: "Home", link: ROUTES.home, icon: <MdHome /> },
+  { name: "Collections", link: ROUTES.collections.index, icon: <FaDatabase /> },
+  { name: "Samples", link: ROUTES.samples, icon: <MdScience /> },
+  {
+    name: "Documentation",
+    link: "https://dev-akashili.github.io/tissue-sample-collection-details/frontend",
+    icon: <FaFileAlt />
+  },
+  {
+    name: "GitHub",
+    link: "https://github.com/Dev-Akashili/tissue-sample-collection-details",
+    icon: <FaGithub />
+  }
+];
