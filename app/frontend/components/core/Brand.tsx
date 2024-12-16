ece07df8e4e5b36e80b9d892d05193ac287d501e
Emeka Akashili <chukwuemeka.akashili@gmail.com>
2024-12-16 07:27:35 +0100

    Update brand and footer

--- app/frontend/components/core/Brand.tsx
+++ app/frontend/components/core/Brand.tsx
@@ -9,9 +9,10 @@ const Brand = () => {
       className="inline-flex justify-between items-center space-x-2 hover:text-blue-500"
     >
       <RiTestTubeFill className="h-6 w-6" />
-      <h1 className="font-bold text-md md:text-lg">
+      <h1 className="font-bold text-md text-lg hidden md:inline-block">
         Tissue Sample Collection Details
       </h1>
+      <h1 className="font-bold text-md text-lg inline-block md:hidden">TSCD</h1>
     </Link>
   );
 };
--- app/frontend/components/core/Footer.tsx
+++ app/frontend/components/core/Footer.tsx
@@ -1,10 +1,11 @@
 export default function Footer() {
   return (
     <footer className="h-20 flex items-center justify-center">
-      <p className="text-xs md:text-sm font-semibold text-center">
-        Digital Research Service developer assessment for RSE role - Emeka
-        Akashili
-      </p>
+      <div className="text-xs md:text-sm font-semibold text-center flex flex-col md:flex-row space-y-1 md:space-y-0 space-x-0 md:space-x-1">
+        <p>Digital Research Service developer assessment for RSE role</p>{" "}
+        <p className="hidden md:inline-block">-</p>
+        <p>Emeka Akashili ({new Date().getFullYear()})</p>
+      </div>
     </footer>
   );
 }
