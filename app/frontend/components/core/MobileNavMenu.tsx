27e24e88bc9f3e6594f13f97b1b1f3c481dab5ce
Emeka Akashili <chukwuemeka.akashili@gmail.com>
2024-12-16 04:26:20 +0100

    Add navbar and home page

---
+++ app/frontend/components/core/MobileNavMenu.tsx
@@ -0,0 +1,59 @@
+import {
+  Sheet,
+  SheetClose,
+  SheetContent,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger
+} from "../ui/sheet";
+import Link from "next/link";
+import { Button } from "../ui/button";
+import Brand from "./Brand";
+import { NAVBAR_LINKS } from "@/constants/Menu.constants";
+
+export const MobileNavMenu = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <Sheet>
+      <SheetTrigger asChild>{children}</SheetTrigger>
+      <SheetContent side={"top"} className="100%">
+        <SheetHeader>
+          <SheetTitle className="text-center">
+            <Brand />
+          </SheetTitle>
+        </SheetHeader>
+        <div className="flex flex-col mt-4">
+          {NAVBAR_LINKS.map((item, index) => (
+            <MobileNavMenuItem
+              key={index}
+              name={item.name}
+              link={item.link}
+              icon={item.icon}
+            />
+          ))}
+        </div>
+      </SheetContent>
+    </Sheet>
+  );
+};
+
+interface MobileNavMenuItem {
+  name: string;
+  link: string;
+  icon: React.ReactNode;
+}
+
+const MobileNavMenuItem = ({ name, link, icon }: MobileNavMenuItem) => {
+  return (
+    <Link href={link}>
+      <SheetClose className="w-full">
+        <Button
+          variant={"ghost"}
+          className="w-full rounded-none hover:bg-gray-300 dark:hover:bg-gray-600 font-semibold"
+        >
+          {icon}
+          {name}
+        </Button>
+      </SheetClose>
+    </Link>
+  );
+};
---
+++ app/frontend/components/core/Navbar.tsx
@@ -0,0 +1,79 @@
+"use client";
+import Link from "next/link";
+import Brand from "./Brand";
+import { Button } from "../ui/button";
+import { NAVBAR_LINKS } from "@/constants/Menu.constants";
+import { useTheme } from "next-themes";
+import { FaRegMoon } from "react-icons/fa";
+import { LuSun } from "react-icons/lu";
+import { MdMenu } from "react-icons/md";
+import { MobileNavMenu } from "./MobileNavMenu";
+
+export const Navbar = () => {
+  const { setTheme } = useTheme();
+
+  return (
+    <div className="h-16 fixed top-0 left-0 right-0 bg-white dark:bg-black px-6 lg:px-20 flex items-center justify-between shadow-md dark:border-b z-10">
+      <Brand />
+      <div className="h-full flex items-center justify-between space-x-4">
+        <div className="h-full hidden lg:flex justify-between">
+          {NAVBAR_LINKS.map((item, index) => (
+            <NavLink
+              key={index}
+              name={item.name}
+              link={item.link}
+              icon={item.icon}
+            />
+          ))}
+        </div>
+        <Button
+          size={"icon"}
+          variant={"outline"}
+          className="hidden dark:flex dark:items-center"
+          onClick={() => setTheme("light")}
+        >
+          <FaRegMoon />
+        </Button>
+        <Button
+          size={"icon"}
+          variant={"outline"}
+          className="dark:hidden"
+          onClick={() => setTheme("dark")}
+        >
+          <LuSun />
+        </Button>
+        <MobileNavMenu>
+          <Button
+            size={"icon"}
+            variant={"outline"}
+            className="inline-block lg:hidden"
+          >
+            <MdMenu className="mx-auto" />
+          </Button>
+        </MobileNavMenu>
+      </div>
+    </div>
+  );
+};
+
+interface NavLinkProps {
+  name: string;
+  link: string;
+  icon?: React.ReactNode;
+}
+
+const NavLink = ({ name, icon, link }: NavLinkProps) => {
+  return (
+    <Link href={link}>
+      <Button
+        variant={"ghost"}
+        className="h-full rounded-none text-sm font-semibold hover:bg-gray-300 dark:hover:bg-gray-600"
+      >
+        {icon}
+        {name}
+      </Button>
+    </Link>
+  );
+};
+
+export default Navbar;
